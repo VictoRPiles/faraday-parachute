fc6ac78e3fa06f6b3efb29fb08c7ea9e831da336
victor <vpildel@upv.edu.es>
2024-02-19 20:42:15 +0100

    FlightData and Rocket classes

---
+++ FlightData.py
@@ -0,0 +1,8 @@
+class FlightData:
+    def __init__(self, height, pressure, temperature, acceleration, position_angles, gps_coordinates):
+        self.height = height
+        self.pressure = pressure
+        self.temperature = temperature
+        self.acceleration = acceleration
+        self.position_angles = position_angles
+        self.gps_coordinates = gps_coordinates
---
+++ Rocket.py
@@ -0,0 +1,3 @@
+class Rocket:
+    def __init__(self):
+        flight_data_history = []
