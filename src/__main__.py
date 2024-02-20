64e2a0ddd1cc9051d5b4c560632fd86ff8de343f
victor <vpildel@upv.edu.es>
2024-02-20 17:27:34 +0100

    Creating the main method

--- src/__main__.py
+++ src/__main__.py
@@ -2,10 +2,15 @@ import logging
 
 from rocket.Rocket import Rocket
 
-if __name__ == '__main__':
+
+def main():
     logging.info("Faraday Rocketry parachute deployment system activated")
 
     rocket = Rocket()
     logging.info(rocket.last_data())
     if rocket.check_engine_shut_down() & rocket.check_altitude_decreasing() & rocket.check_pointing_downwards():
         rocket.release_parachute()
+
+
+if __name__ == '__main__':
+    main()
